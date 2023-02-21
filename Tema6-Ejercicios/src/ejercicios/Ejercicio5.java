9711790b3a93817f8b72b533815ba98442d4b369
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-02-21 12:45:03 +0100

    Empezado

---
+++ Tema6-Ejercicios/src/ejercicios/Ejercicio5.java
@@ -0,0 +1,21 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+/**
+ * Introducir por teclado una frase palabra a palabra, y mostrar la frase
+ * completa separando las palabras introducidas con espacios en blanco. Terminar
+ * de leer la frase cuando alguna de las palabras introducidas sea la cadena
+ * “fin” escrita con cualquier combinación de mayúsculas/minúsculas. La cadena
+ * “fin” no aparecerá en la frase final.
+ * 
+ * @author fjmiguel
+ *
+ */
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner (System.in);
+	}
+
+}
