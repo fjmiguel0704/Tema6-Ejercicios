05112e0dfb02ef53c7d1821e7fc7d8c1e1f63fd0
Fernando <Fernando@DESKTOP-3EE8PP0.home>
2023-02-28 22:26:09 +0100

    Funciones Ejercicio 9

---
+++ Tema6-Ejercicios/src/metodos/MetodoEj9.java
@@ -0,0 +1,13 @@
+package metodos;
+
+
+public class MetodoEj9 {
+
+	char codifica(char conjunto1[], char conjunto2[], char c) {
+		for (int i = 0; i < conjunto1.length; i++) {
+				
+		}
+		return c;
+	}
+
+}
