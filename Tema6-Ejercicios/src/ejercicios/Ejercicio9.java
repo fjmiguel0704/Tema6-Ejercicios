c64748f8afa3bbb1091e188eab4a3d8aa0aff94e
Fernando <Fernando@DESKTOP-3EE8PP0.home>
2023-02-28 22:26:20 +0100

    Comenzado

---
+++ Tema6-Ejercicios/src/ejercicios/Ejercicio9.java
@@ -0,0 +1,18 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+	// Creamos el scanner para leer por teclado
+	static public Scanner read = new Scanner(System.in);
+
+	public static void main(String[] args) { 
+		//Definimos el primer conjunto de caracteres 
+		char conjunto1 [] = {'e', 'i', 'k', 'm', 'p', 'q', 'r', 's', 't', 'u', 'v'};
+		
+		//Definimos el segundo conjunto de caracteres 
+		char conjunto2 [] = {'p', 'v', 'i',	'u', 'm', 't', 'e',	'r', 'k', 'q', 's'};
+
+	}
+
+}
