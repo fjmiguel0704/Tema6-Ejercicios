9490ae4bad66a4fe462dfa34c24a88956b612d8a
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-03-06 10:45:13 +0100

    Hacer

---
+++ Tema6-Ejercicios/src/ejercicios/Ejercicio15.java
@@ -0,0 +1,15 @@
+package ejercicios;
+
+public class Ejercicio15 {
+	/**
+	 * Modificar el ejercicio anterior para que el programa indique al jugador 2, en
+	 * cada intento, cuántas letras coinciden con el texto original.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
